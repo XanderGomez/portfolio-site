ed55d255c2fe14ce65141346ca80c503ebbde202
Xander Gomez <alextgomez21@gmail.com>
2026-04-21 09:41:19 -0500

    Added basic routing and pages for future development. Also updated system dependencies and style file locations.

---
+++ src/pages/About.tsx
@@ -0,0 +1,16 @@
+
+
+function About() {
+    return (
+        <div>
+            <h1>About Me!</h1>
+            <ul>
+                <li>Name: Alexander(Xander) Gomez</li>
+                <li>Age: 21</li>
+                <li>Hobbies: Building Computers, Playing Video Games, Skiing, and much more</li>
+            </ul>
+        </div>
+    );
+}
+
+export default About;
