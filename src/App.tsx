1d6f8fd9b901ee74f79d015845a98614f80afef8
Xander Gomez <alextgomez21@gmail.com>
2026-04-22 09:43:16 -0500

    Removed unecessary image files from vite setup and added a navbar component for navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+function Navbar() {
+    return (
+        <nav >
+            <h2>My Portfolio</h2>
+
+            <div className="flex-container">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
