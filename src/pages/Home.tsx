2912059f8550d1b10e0961bae95da64b869ed985
Xander Gomez <alextgomez21@gmail.com>
2026-04-23 10:21:47 -0500

    Included changes to the Home.tsx from last commit.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,20 @@
 
 function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the home page!</p>
-        </div>
+        <main>
+            <section>
+                <h1>Xander Gomez</h1>
+                <p>
+                    Computer Science & Cybersecurity student focused on building web
+                    applications and growing in software development.
+                </p>
+                <p>
+                    Currently studying at the University of Nebraska at Omaha and working
+                    as a DevOps intern at Fiserv.
+                </p>
+            </section>
+        </main>
+        
     );
 }
 
