b971967ebbe55e545a3c86db692afff3d3d5f85f
Xander Gomez <alextgomez21@gmail.com>
2026-04-26 23:03:02 -0500

    Listed two personal Projects w/ git repos on projects page.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,12 +4,24 @@ function Projects() {
     return (
         <main>
             <section>
-                <h1> This Page Showcases Personal Projects </h1>
+                <h1>Personal Projects</h1>
 
+                <h2>Project 1: Stats Projcet</h2>
                 <ul>
-                    <li>Project 1</li>
-                    <li>Project 2</li>
-                    <li>Project 3</li>
+                    <li>Made in Python</li>
+                    <li>Created to aid in Statistics Class</li>
+                    <li>Work in Progress</li>
+                    <li><a href="https://github.com/XanderGomez/Statistics-Calculator" target="_blank">Git Repo</a></li>
+                </ul>
+
+                <br></br>
+
+                <h2>Project 2: MavMarketplace</h2>
+                <ul>
+                    <li>Made using React</li>
+                    <li>Made for Software Engineering class</li>
+                    <li>Open to future improvements</li>
+                    <li><a href="https://github.com/CSCI4830-UNO/MavMarketplace" target="_blank">Git Repo</a></li>
                 </ul>
             </section>
         </main>
