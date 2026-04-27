dc3fdfe884525f69dba5ef53c45a81d4ffdefc67
Xander Gomez <alextgomez21@gmail.com>
2026-04-27 09:27:24 -0500

    Updated Proojects Page structure and styling, changed index.css to personal style over pre-installed style, and added bottom margin to navbar.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-
+import "../styles/Projects.css"
 
 function Projects() {
     return (
@@ -6,23 +6,26 @@ function Projects() {
             <section>
                 <h1>Personal Projects</h1>
 
-                <h2>Project 1: Stats Projcet</h2>
-                <ul>
-                    <li>Made in Python</li>
-                    <li>Created to aid in Statistics Class</li>
-                    <li>Work in Progress</li>
-                    <li><a href="https://github.com/XanderGomez/Statistics-Calculator" target="_blank">Git Repo</a></li>
-                </ul>
+                <div className="projects-container">
+                    <div className="project-card">
+                        <h2>Project 1: Stats Project</h2>
 
-                <br></br>
+                        <p><strong>Tech:</strong> Python</p>
+                        <p><strong>Purpose:</strong> Aid for statistics course work</p>
+                        <p><strong>Status:</strong> Work in Progress</p>
 
-                <h2>Project 2: MavMarketplace</h2>
-                <ul>
-                    <li>Made using React</li>
-                    <li>Made for Software Engineering class</li>
-                    <li>Open to future improvements</li>
-                    <li><a href="https://github.com/CSCI4830-UNO/MavMarketplace" target="_blank">Git Repo</a></li>
-                </ul>
+                        <a href="https://github.com/XanderGomez/Statistics-Calculator" target="_blank">View Repository</a>
+                    </div>
+                    <div className="project-card">
+                        <h2>Project 2: MavMarketplace</h2>
+
+                        <p><strong>Tech:</strong> React</p>
+                        <p><strong>Purpose:</strong> Software Engineering Course Project</p>
+                        <p><strong>Status:</strong> Possible Visit to Add Finishing Touches</p>
+
+                        <a href="https://github.com/CSCI4830-UNO/MavMarketplace" target="_blank">View Repository</a>
+                    </div>
+                </div>              
             </section>
         </main>
     )
